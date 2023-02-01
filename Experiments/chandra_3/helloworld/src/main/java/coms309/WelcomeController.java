0b0443087518016baf8224239f3a2b67a6083d78
Chandra <tcsrxstar@gmail.com>
2023-02-01 16:18:31 -0600

    origin

---
+++ Experiments/chandra_3/helloworld/src/main/java/coms309/WelcomeController.java
@@ -0,0 +1,20 @@
+package coms309;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309: " + name;
+    }
+
+
+}
+
