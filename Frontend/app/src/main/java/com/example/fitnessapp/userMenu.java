3bdde7bce48e53b7a203ef15bca4c9d023901855
Tianyi <lty228@iastate.edu>
2023-02-09 17:54:41 -0600

    2/9/2023

--- Frontend/app/src/main/java/com/example/fitnessapp/userMenu.java
+++ Frontend/app/src/main/java/com/example/fitnessapp/userMenu.java
@@ -2,7 +2,10 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class userMenu extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class userMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_menu);
+        Button logOutBtn = findViewById(R.id.logOutBtn);
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logOut = new Intent(userMenu.this, MainActivity.class);
+                startActivity(logOut);
+            }
+        });
+        Button recordBtn = findViewById(R.id.recordBtn);
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toRecord = new Intent(userMenu.this, record.class);
+                startActivity(toRecord);
+            }
+        });
     }
 }
