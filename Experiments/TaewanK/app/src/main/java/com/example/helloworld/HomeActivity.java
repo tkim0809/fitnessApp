581f59e894752f7bc92a919584726497a7bd5d66
taewankim <taewank@iastate.edu>
2023-03-01 04:49:51 -0600

    login page

--- Experiments/TaewanK/app/src/main/java/com/example/helloworld/HomeActivity.java
+++ Experiments/TaewanK/app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -7,6 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -42,11 +52,60 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String name = username.getText().toString();
                 String pw = password.getText().toString();
+                userName = name;
+                String url="";
 
-                Intent intent = new Intent(HomeActivity.this, loginActivity.class);
-                intent.putExtra("keyUsername", name);
-                intent.putExtra("keyPassword", pw);
-                startActivity(intent);
+                JsonArrayRequest jsonArr = new JsonArrayRequest(Request.Method.GET, url,
+                        null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                try {
+
+                                    for (int i = 0; i < response.length(); i++) {
+
+                                        JSONObject user = response.getJSONObject(i);
+
+                                        String BEUsername = user.getString("userName");
+                                        String BEPassword = user.getString("password");
+
+                                        if (name.equals(BEUsername) && pw.equals(BEPassword)) {
+
+                                            if (name.equals("user") && pw.equals("user")) {
+
+                                                System.out.println("Login is successful");
+                                                Intent intent = new Intent(HomeActivity.this, loginActivity.class);
+                                                intent.putExtra("keyUsername", name);
+                                                intent.putExtra("keyPassword", pw);
+                                                startActivity(intent);
+                                                break;
+
+                                            } else {
+
+                                                System.out.println("Login failed");
+
+                                            }
+
+                                        }
+
+                                    }
+
+                                } catch (JSONException e) {
+
+                                    e.printStackTrace();
+
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                );
+
+
+
             }
         });
 
