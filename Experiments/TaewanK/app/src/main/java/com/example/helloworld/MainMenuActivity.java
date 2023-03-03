22ece398252929d4edc6cb2d75fb399920f2841d
taewankim <taewank@iastate.edu>
2023-03-03 04:40:56 -0600

    commit for branch

--- Experiments/TaewanK/app/src/main/java/com/example/helloworld/MainMenuActivity.java
+++ Experiments/TaewanK/app/src/main/java/com/example/helloworld/MainMenuActivity.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,12 +19,40 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        Button friendButton = findViewById(R.id.findFriendButton);
+        Button friendListButton = findViewById(R.id.FriendListButtonXML);
+
+        friendButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainMenuActivity.this, findFriendActivity.class);
+                startActivity(i);
+
+            }
+
+        });
+
+        friendListButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainMenuActivity.this, FriendListActivity.class);
+                startActivity(i);
+            }
+        }) ;
+
         TextView welcomeMsg = findViewById(R.id.welcomeMessage);
 
         Bundle extras = getIntent().getExtras();
         String email = extras.getString("email");
         String password = extras.getString("password");
         welcomeMsg.setText("Welcome " + email);
+
+
 
 
 
---
+++ Experiments/TaewanK/app/src/main/java/com/example/helloworld/findFriendActivity.java
@@ -0,0 +1,150 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class findFriendActivity extends AppCompatActivity {
+    public ArrayList<String> friendEmailList = new ArrayList<String>();
+    int friendNumber = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+        EditText emailEditText = findViewById(R.id.friendEmail);
+        Button findButton = findViewById(R.id.findFriendButton);
+        Button backToMenuBtn = findViewById(R.id.backToMenu);
+
+        backToMenuBtn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(findFriendActivity.this, MainMenuActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        String email = emailEditText.getText().toString();
+
+        findButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                String url = "http://coms-309-004.class.las.iastate.edu:8080/login";
+                RequestQueue queue = Volley.newRequestQueue(findFriendActivity.this);
+                JSONObject obj = new JSONObject();
+
+                try {
+
+                    obj.put("email", email);
+
+
+                } catch (Exception e) {
+
+                    System.out.println("ERROR");
+
+                }
+
+                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                        url, obj,
+                        new Response.Listener<JSONObject>() {
+
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+
+                                try {
+
+                                    if (response.getBoolean("email")) {
+
+                                        Toast.makeText(findFriendActivity.this, "Successful added to friend list", Toast.LENGTH_LONG).show();
+                                        //Intent i = new Intent(findFriendActivity.this, FriendListActivity.class);
+                                        //i.putExtra("friendNumber", "friendNumber");
+                                        //i.putExtra("email"+friendNumber, "email");
+                                        friendNumber++;
+                                        friendEmailList.add(obj.get("email").toString());
+
+                                    }
+
+                                } catch (JSONException e) {
+
+                                    throw new RuntimeException(e);
+
+                                }
+
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error instanceof ServerError) {
+                            Toast.makeText(findFriendActivity.this, "Server", Toast.LENGTH_SHORT).show();
+                        } else if (error instanceof NetworkError) {
+                            Toast.makeText(findFriendActivity.this, "Network", Toast.LENGTH_SHORT).show();
+                            System.out.println(error.getMessage());
+                        } else if (error instanceof AuthFailureError) {
+                            Toast.makeText(findFriendActivity.this, "Auth", Toast.LENGTH_SHORT).show();
+                        } else if (error instanceof ParseError) {
+                            Toast.makeText(findFriendActivity.this, "Parse", Toast.LENGTH_SHORT).show();
+                        } else if (error instanceof NoConnectionError) {
+                            Toast.makeText(findFriendActivity.this, "No Connection", Toast.LENGTH_SHORT).show();
+                        } else if (error instanceof TimeoutError) {
+                            Toast.makeText(findFriendActivity.this, "T/O", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(findFriendActivity.this, "Unknown", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }) {
+
+                    @Override
+                    public Map<String, String> getParams() {
+
+                        Map<String, String> params = new HashMap<String, String>();
+                        return params;
+
+                    }
+
+                };
+
+                queue.add(jsonObjReq);
+
+            }
+
+
+        });
+
+
+    }}
