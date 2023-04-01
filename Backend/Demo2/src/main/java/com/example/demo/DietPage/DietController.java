7c4925056ba0aa318022652379145da5e8c80ce6
chandra3 <chandra3@iastate.edu>
2023-04-01 00:18:33 -0500

    Added Autowired

--- Backend/Demo2/src/main/java/com/example/demo/DietPage/DietController.java
+++ Backend/Demo2/src/main/java/com/example/demo/DietPage/DietController.java
@@ -3,12 +3,14 @@ package com.example.demo.DietPage;
 import com.example.demo.appuser.AppUser;
 import com.example.demo.appuser.AppUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping(path = "/diet")
 public class DietController {
+    @Autowired
 
     private final DietRepository dietRepository;
     private final AppUserService appUserService;
