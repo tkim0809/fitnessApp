96c12412a89b1e3c2354fd0f19e4155ef3062c51
eddylty0228 <lty228eddy@gmail.com>
2023-04-03 19:31:07 -0500

    diet goal page

---
+++ Frontend/app/src/main/java/com/example/fitnessapp/DietGoal.java
@@ -0,0 +1,71 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.fitnessapp.Logic.IVolleyListener;
+import com.example.fitnessapp.Logic.dietGoalLogic;
+import com.example.fitnessapp.Network.IServerRequest;
+import com.example.fitnessapp.Network.updateRequests;
+
+import org.json.JSONException;
+
+public class DietGoal extends AppCompatActivity implements IView{
+    int weeklyCalNum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_goal);
+        EditText inputCal = findViewById(R.id.goalCalTxt);
+        TextView weeklyCal = findViewById(R.id.weeklyCalTxt);
+        Button setBtn = findViewById(R.id.setBtn);
+        inputCal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This method is called before the text is changed.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // This method is called when the text is changed.
+                String text = s.toString();
+                weeklyCalNum = Integer.parseInt(text)*7;
+                weeklyCal.setText(weeklyCalNum+"Cals");
+                // Use the text variable here.
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This method is called after the text is changed.
+            }
+        });
+        IServerRequest request = new updateRequests();
+        final dietGoalLogic logic = new dietGoalLogic(DietGoal.this,request);
+        setBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dayCal = inputCal.getText().toString();
+                String weekCal = String.valueOf(weeklyCalNum);
+                try {
+                    logic.setGoal(dayCal,weekCal);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void showText(String s) {
+
+    }
+}
---
+++ Frontend/app/src/main/java/com/example/fitnessapp/Logic/dietGoalLogic.java
@@ -0,0 +1,34 @@
+package com.example.fitnessapp.Logic;
+
+import com.example.fitnessapp.IView;
+import com.example.fitnessapp.Network.IServerRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class dietGoalLogic implements IVolleyListener{
+    IView i;
+    IServerRequest serverRequest;
+
+    public dietGoalLogic(IView i, IServerRequest serverRequest) {
+        this.i = i;
+        this.serverRequest = serverRequest;
+        serverRequest.addVolleyListener(this);
+    }
+    public void setGoal(String dailyCal, String weeklyCal) throws JSONException {
+        final String url ="TBD";
+        JSONObject goal = new JSONObject();
+        goal.put("dailyGoal",dailyCal);
+        goal.put("weeklyGoal",weeklyCal);
+        serverRequest.sendToServer(url,goal,"Put");
+    }
+    @Override
+    public void onSuccess() {
+
+    }
+
+    @Override
+    public void onError() {
+
+    }
+}
