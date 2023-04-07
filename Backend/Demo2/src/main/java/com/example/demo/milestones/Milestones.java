08a89fa5a6e1eed5bc203a2df330ee8c3fc87b5d
Anirudha <asc@iastate.edu>
2023-04-07 17:10:23 -0500

    origin

--- Backend/Demo2/src/main/java/com/example/demo/milestones/Milestones.java
+++ Backend/Demo2/src/main/java/com/example/demo/milestones/Milestones.java
@@ -94,9 +94,9 @@ public class Milestones {
         return new ToStringCreator(this)
 
                 .append("id", this.getId())
-                .append("workoutName", this.getMilestoneName())
-                .append("weights", this.getMilestoneWeight())
-                .append("reps", this.getMilestoneReps())
-                .append("sets", this.getMilestoneSets()).toString();
+                .append("milestone_name", this.getMilestoneName())
+                .append("milestone_reps", this.getMilestoneReps())
+                .append("milestone_sets", this.getMilestoneSets())
+                .append("milestone_weight", this.getMilestoneWeight()).toString();
     }
 }
