b23cfabe147f53d6e3adb03710255da05f461cb7
Anirudha <asc@iastate.edu>
2023-04-09 04:02:42 -0500

    added userId to milestones, added milestone status, added delete milestone method

--- Backend/Demo2/src/main/java/com/example/demo/milestones/Milestones.java
+++ Backend/Demo2/src/main/java/com/example/demo/milestones/Milestones.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 import org.hibernate.annotations.Table;
 import org.springframework.core.style.ToStringCreator;
+
 import javax.persistence.*;
 
 @Entity
@@ -31,6 +32,14 @@ public class Milestones {
     @NotFound(action = NotFoundAction.IGNORE)
     private String milestone_weight;
 
+    @Column(name = "completed")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private boolean completed;
+
+    @Column(name = "user_id")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private Integer userId;
+
     public Milestones() {
 
     }
@@ -40,13 +49,17 @@ public class Milestones {
         this.milestone_sets = milestone_sets;
         this.milestone_reps = milestone_reps;
         this.milestone_weight = "N/A";
+        this.completed = false;
+        this.userId = userId;
     }
 
-    public Milestones(String milestone_name, String milestone_sets, String milestone_reps, String milestone_weight) {
+    public Milestones(String milestone_name, String milestone_sets, String milestone_reps, String milestone_weight, boolean completed, Integer userId) {
         this.milestone_name = milestone_name;
         this.milestone_sets = milestone_sets;
         this.milestone_reps = milestone_reps;
         this.milestone_weight = milestone_weight;
+        this.completed = completed;
+        this.userId = userId;
     }
 
     public Integer getId() {
@@ -89,14 +102,32 @@ public class Milestones {
         this.milestone_weight = milestone_weight;
     }
 
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
     @Override
     public String toString() {
         return new ToStringCreator(this)
-
                 .append("id", this.getId())
+                .append("userId", this.getUserId())
                 .append("milestoneName", this.getMilestoneName())
                 .append("milestoneSets", this.getMilestoneSets())
                 .append("milestoneReps", this.getMilestoneReps())
-                .append("milestoneWeight", this.getMilestoneWeight()).toString();
+                .append("milestoneWeight", this.getMilestoneWeight())
+                .append("completed", this.isCompleted()).toString();
     }
+
 }
--- Backend/Demo2/src/main/java/com/example/demo/milestones/MilestonesRepository.java
+++ Backend/Demo2/src/main/java/com/example/demo/milestones/MilestonesRepository.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface MilestonesRepository extends JpaRepository<Milestones, Integer> {
+    List<Milestones> findAllByUserId(Integer userId);
 
 }
