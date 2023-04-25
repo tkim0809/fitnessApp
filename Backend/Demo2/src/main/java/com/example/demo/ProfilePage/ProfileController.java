338d92f79d76853cb1100abe7fdab667fd3286c7
chandra3 <chandra3@iastate.edu>
2023-04-25 11:51:18 -0500

    Worked on SwaggerConfig for Registration, Profile, Milestone, Leaderboards, Friends, Stats

--- Backend/Demo2/src/main/java/com/example/demo/ProfilePage/ProfileController.java
+++ Backend/Demo2/src/main/java/com/example/demo/ProfilePage/ProfileController.java
@@ -6,7 +6,13 @@ import com.example.demo.appuser.AppUser;
 import com.example.demo.appuser.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
+@Api(tags = "Profile")
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
@@ -17,21 +23,46 @@ public class ProfileController {
     @Autowired
     private ProfileRepository profileRepository;
 
+    @ApiOperation(value = "Get user profile")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Profile retrieved successfully")
+    })
     @GetMapping("/{userId}")
-    public Profile getProfile(@PathVariable Long userId) {
+    public Profile getProfile(
+            @ApiParam(value = "User ID", required = true)
+            @PathVariable Long userId
+    ) {
         return profileRepository.findByUser_Id(userId);
     }
 
+    @ApiOperation(value = "Create user profile")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Profile created successfully")
+    })
     @PostMapping("/{userId}")
-    public Profile createProfile(@PathVariable Long userId, @RequestBody Profile profile) {
+    public Profile createProfile(
+            @ApiParam(value = "User ID", required = true)
+            @PathVariable Long userId,
+            @ApiParam(value = "Profile object containing profile details", required = true)
+            @RequestBody Profile profile
+    ) {
         AppUser user = appUserRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
         profile.setUser(user);
         return profileRepository.save(profile);
     }
 
+    @ApiOperation(value = "Update user profile")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Profile updated successfully")
+    })
     @PutMapping("/{userId}")
-    public Profile updateProfile(@PathVariable Long userId, @RequestBody Profile updatedProfile) {
+    public Profile updateProfile(
+            @ApiParam(value = "User ID", required = true)
+            @PathVariable Long userId,
+            @ApiParam(value = "Updated profile object containing new profile details", required = true)
+            @RequestBody Profile updatedProfile
+    ) {
         Profile profile = profileRepository.findByUser_Id(userId);
 
         if (updatedProfile.getGender() != null) {
