e6c13dce6e205b9e5f349906ee3368184bfe17a4
chandra3 <chandra3@iastate.edu>
2023-04-28 19:28:34 -0500

    AddFriends

--- Backend/Demo2/src/main/java/com/example/demo/AddFriends/FriendsController.java
+++ Backend/Demo2/src/main/java/com/example/demo/AddFriends/FriendsController.java
@@ -59,17 +59,14 @@ public class FriendsController {
         }
         AppUser friend = appUserRepository.findByEmail(friendEmail)
                 .orElseThrow(() -> new org.springframework.security.core.userdetails.UsernameNotFoundException("User not found with email: " + friendEmail));
-        Friends newFriendship1 = new Friends(user, friend.getId()); // Current user to friend relationship
-        Friends newFriendship2 = new Friends(friend, user.getId()); // Friend to current user relationship
-        friendsRepository.save(newFriendship1);
-        friendsRepository.save(newFriendship2);
+        Friends newFriendship = new Friends(user, friend.getId());
+        friendsRepository.save(newFriendship);
 
         // Send a notification to the new friend
         webSocketServer.sendMessageToPArticularUser(friend.getId().toString(), "User with ID: " + userId + " added you as a friend.");
 
         return "{\"message\" : \"success\"}";
     }
-
 
 
 
@@ -92,5 +89,4 @@ public class FriendsController {
 
 
 }
-
 
