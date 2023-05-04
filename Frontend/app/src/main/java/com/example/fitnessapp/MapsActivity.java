f1a356e72013751ef80e0d44c54a282ea242334d
tcsr1234 <tcsrxstar@gmail.com>
2023-05-03 21:35:18 -0500

    Worked on Map feature

--- Frontend/app/src/main/java/com/example/fitnessapp/MapsActivity.java
+++ Frontend/app/src/main/java/com/example/fitnessapp/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.fitnessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,6 +13,9 @@ import com.example.fitnessapp.R;
 import android.Manifest;
 import androidx.core.content.ContextCompat;
 import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.core.app.ActivityCompat;
 
 
@@ -24,14 +28,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     public static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
 
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Button backBtn = findViewById(R.id.backButton);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MapsActivity.this, NewUserMenu.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
