4475ea79b8b07c7a7ef5ebfef8bdeae5d082a4bb
taewankim <taewank@iastate.edu>
2023-05-06 04:51:50 -0500

    testcases

--- Frontend/app/src/androidTest/java/com/example/fitnessapp/LoginPageTest.java
+++ Frontend/app/src/androidTest/java/com/example/fitnessapp/LoginPageTest.java
@@ -1,91 +1,52 @@
 package com.example.fitnessapp;
 
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@LargeTest
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(AndroidJUnit4.class)
 public class LoginPageTest {
 
     @Rule
-    public ActivityScenarioRule<LoginPage> mActivityScenarioRule =
-            new ActivityScenarioRule<>(LoginPage.class);
+    public ActivityScenarioRule<LoginPage> activityRule = new ActivityScenarioRule<>(LoginPage.class);
 
     @Test
-    public void loginPageTest() {
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.username),
-                        childAtPosition(
-                                allOf(withId(R.id.login_page),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("testEspresso@gmail.com"), closeSoftKeyboard());
+    public void testLoginButton() {
+        // Enter email
+        onView(withId(R.id.username)).perform(typeText("testEspresso@gmail.com"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.password),
-                        childAtPosition(
-                                allOf(withId(R.id.login_page),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                2),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("1234567890"), closeSoftKeyboard());
+        // Enter password
+        onView(withId(R.id.password)).perform(typeText("1234567890"), closeSoftKeyboard());
 
-        ViewInteraction materialButton = onView(
-                allOf(withId(R.id.loginButton), withText("Log in"),
-                        childAtPosition(
-                                allOf(withId(R.id.login_page),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                3),
-                        isDisplayed()));
-        materialButton.perform(click());
+        // Click login button
+        onView(withId(R.id.loginButton)).perform(click());
+
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+    @Test
+    public void testLoginButtonInvalidCredentials() {
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
+        // Enter incorrect email
+        onView(withId(R.id.username)).perform(typeText("wrong@example.com"), closeSoftKeyboard());
 
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        // Enter incorrect password
+        onView(withId(R.id.password)).perform(typeText("wrongPassword"), closeSoftKeyboard());
+
+        // Click login button
+        onView(withId(R.id.loginButton)).perform(click());
+
     }
 }
